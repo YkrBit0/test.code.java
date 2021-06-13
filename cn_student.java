56f0027e99e28037f0cdec27b0c51d7d38fe067f
YkrBit0 <83524483+YkrBit0@users.noreply.github.com>
2021-06-13 21:56:44 +0800

    added a file

---
+++ cn_student.java
@@ -0,0 +1,54 @@
+package Set_集合;
+
+import java.util.Objects;
+
+public class cn_student {
+    private String name;
+    private int age;
+
+    public cn_student() {
+    }
+
+    public cn_student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "cn_student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        cn_student that = (cn_student) o;
+        return age == that.age && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+}
