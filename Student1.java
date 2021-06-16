435059e756c7d367b6798312e97495dd5d0ec02a
YkrBit0 <83524483+YkrBit0@users.noreply.github.com>
2021-06-16 22:08:01 +0800

    added a file

---
+++ Student1.java
@@ -0,0 +1,53 @@
+package map_集合;
+
+import java.util.Objects;
+
+public class Student1 {
+    private String name;
+    private int age;
+
+    public Student1() {
+    }
+
+    public Student1(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student1{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student1 student1 = (Student1) o;
+        return age == student1.age && Objects.equals(name, student1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
